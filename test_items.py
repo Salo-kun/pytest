d82cea397de10a7fd2ac7bd3c251f586ecb4bc85
Andrii Avramuk <avramuk.andrii@gmail.com>
2020-09-14 11:10:43 +0300

    change css selector

--- test_items.py
+++ test_items.py
@@ -1,8 +1,7 @@
 def test_find_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     try:
-        browser.find_element_by_css_selector('[type="submit"].btn-lg')
+        browser.find_element_by_css_selector('.btn-add-to-basket')
     except:
         assert False, 'cart button not found'
 
-        
